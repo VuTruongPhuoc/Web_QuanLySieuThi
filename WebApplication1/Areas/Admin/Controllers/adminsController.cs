56b224ff97ac17dbab958ecdb82137c0e23ee769
Marin_Phuoc <vuphuoc7722@gmail.com>
2023-05-29 01:35:44 +0700

    update

---
+++ WebApplication1/Areas/Admin/Controllers/adminsController.cs
@@ -0,0 +1,62 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    public class adminsController : Controller
+    {
+        private JP_QuanLySieuThiEntities1 db = new JP_QuanLySieuThiEntities1();
+
+        // GET: Admin/admins
+        public ActionResult Index()
+        {
+            
+            return View(db.admins.ToList());
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
